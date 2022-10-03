0a5ed9efe5ceb21e238fafc914a7016937072aa0
zaffiroblu <jenshivel@gmail.com>
2022-10-03 19:15:33 +0200

    Make responsive version of WV-Website with tabs generated by JS

--- main.js
+++ main.js
@@ -7,26 +7,242 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./sass/main.scss":
+/*!************************!*\
+  !*** ./sass/main.scss ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://wv-berlin/./sass/main.scss?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log('Things seem to be working!');\n\n\n//# sourceURL=webpack://wv-berlin/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../sass/main.scss */ \"./sass/main.scss\");\n/* harmony import */ var _src_assets_images_vw_berlin_logo_big_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../../../../src/assets/images/vw-berlin-logo-big.png */ \"./src/assets/images/vw-berlin-logo-big.png\");\n/* harmony import */ var _src_assets_images_wv_logo_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../../../src/assets/images/wv-logo.png */ \"./src/assets/images/wv-logo.png\");\n/* harmony import */ var _src_assets_images_florian_wehde_1uWanmgkd5g_unsplash_jpg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../../../../src/assets/images/florian-wehde-1uWanmgkd5g-unsplash.jpg */ \"./src/assets/images/florian-wehde-1uWanmgkd5g-unsplash.jpg\");\n/* harmony import */ var _src_assets_images_ian_schneider_PAykYb_8Er8_unsplash_jpg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../../../../../src/assets/images/ian-schneider-PAykYb-8Er8-unsplash.jpg */ \"./src/assets/images/ian-schneider-PAykYb-8Er8-unsplash.jpg\");\n/* harmony import */ var _src_assets_images_nathan_dumlao_6VhPY27jdps_unsplash_jpg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../../../../../src/assets/images/nathan-dumlao-6VhPY27jdps-unsplash.jpg */ \"./src/assets/images/nathan-dumlao-6VhPY27jdps-unsplash.jpg\");\n/* harmony import */ var _src_assets_images_bonnie_kittle_9_nse_cRvkE_unsplash_jpg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../../../../../../src/assets/images/bonnie-kittle-9-nse_cRvkE-unsplash.jpg */ \"./src/assets/images/bonnie-kittle-9-nse_cRvkE-unsplash.jpg\");\n/* harmony import */ var _src_assets_images_glenn_carstens_peters_npxXWgQ33ZQ_unsplash_jpg__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../../../../../../src/assets/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg */ \"./src/assets/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg\");\n/* harmony import */ var _loadPage1__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./loadPage1 */ \"./src/loadPage1.js\");\n/* harmony import */ var _pages_page1__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./pages/page1 */ \"./src/pages/page1.js\");\n/* harmony import */ var _pages_page2__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./pages/page2 */ \"./src/pages/page2.js\");\n/* harmony import */ var _pages_page3__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./pages/page3 */ \"./src/pages/page3.js\");\n/* harmony import */ var _pages_page4__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./pages/page4 */ \"./src/pages/page4.js\");\n\n\n\n\n\n\n\n\n\n\nonload = (0,_loadPage1__WEBPACK_IMPORTED_MODULE_8__[\"default\"])();\n\n\n\n\n\nwindow.goToPageFour = _pages_page4__WEBPACK_IMPORTED_MODULE_12__[\"default\"];\n\nconst startSeite = document.getElementById('home-tab');\nconst aboutUs = document.getElementById('about-us-tab');\nconst kontakt = document.getElementById('contact-tab');\n// const impressumBtn1 = document.getElementById('impressum-btn-1');\n// const allImpressumBtns = document.querySelectorAll('.impressum-btn');\n\nstartSeite.addEventListener('click', _pages_page1__WEBPACK_IMPORTED_MODULE_9__[\"default\"]);\n\naboutUs.addEventListener('click', _pages_page2__WEBPACK_IMPORTED_MODULE_10__[\"default\"]);\n\nkontakt.addEventListener('click', _pages_page3__WEBPACK_IMPORTED_MODULE_11__[\"default\"]);\n\n\n//# sourceURL=webpack://wv-berlin/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/loadPage1.js":
+/*!**************************!*\
+  !*** ./src/loadPage1.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ loadPageOne)\n/* harmony export */ });\nfunction loadPageOne() {\n\t//This should clone and append the 1st page\n\tlet templatePage1Load = document.getElementById('page1');\n\tlet clonePage1Load = templatePage1Load.content.cloneNode(true);\n\tlet mainContainer = document.getElementById('main-container');\n\tmainContainer.appendChild(clonePage1Load);\n\n\twindow.scrollTo(0, 0);\n}\n\n\n//# sourceURL=webpack://wv-berlin/./src/loadPage1.js?");
+
+/***/ }),
+
+/***/ "./src/pages/page1.js":
+/*!****************************!*\
+  !*** ./src/pages/page1.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ generatePageOne)\n/* harmony export */ });\nfunction generatePageOne() {\n\t// This deactivates the tab from before\n\tlet previousActiveTab = document.querySelector('.active');\n\tpreviousActiveTab.classList.remove('active');\n\tconsole.log(previousActiveTab);\n\n\t// This activates the new tab\n\tlet currentActiveTab = document.querySelector('#home-tab');\n\tcurrentActiveTab.classList.add('active');\n\tconsole.log(currentActiveTab);\n\n\t// This removes all content from the previous page:\n\tlet allContent = document.querySelectorAll('.content');\n\tallContent.forEach((item) => {\n\t\titem.remove();\n\t});\n\n\t//This should clone and append the 1st page:\n\tconst templatePage1 = document.getElementById('page1');\n\tconst clonePage1 = templatePage1.content.cloneNode(true);\n\tconst mainContainer = document.getElementById('main-container');\n\tmainContainer.appendChild(clonePage1);\n\n\twindow.scrollTo({ top: 0, left: 0, behavior: 'instant' });\n}\n\n\n//# sourceURL=webpack://wv-berlin/./src/pages/page1.js?");
+
+/***/ }),
+
+/***/ "./src/pages/page2.js":
+/*!****************************!*\
+  !*** ./src/pages/page2.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ generatePageTwo)\n/* harmony export */ });\nfunction generatePageTwo() {\n\t// This deactivates the tab from before\n\tlet previousActiveTab = document.querySelector('.active');\n\tpreviousActiveTab.classList.remove('active');\n\n\t// This activates the new tab\n\tlet currentActiveTab = document.querySelector('#about-us-tab');\n\tcurrentActiveTab.classList.add('active');\n\n\t// This removes all content from the previous page:\n\tlet allContent = document.querySelectorAll('.content');\n\tallContent.forEach((item) => {\n\t\titem.remove();\n\t});\n\n\t//This should clone and append the 2nd page:\n\tconst templatePage2 = document.getElementById('page2');\n\tconst clonePage2 = templatePage2.content.cloneNode(true);\n\tconst mainContainer = document.getElementById('main-container');\n\tmainContainer.appendChild(clonePage2);\n\n\twindow.scrollTo({ top: 0, left: 0, behavior: 'instant' });\n}\n\n\n//# sourceURL=webpack://wv-berlin/./src/pages/page2.js?");
+
+/***/ }),
+
+/***/ "./src/pages/page3.js":
+/*!****************************!*\
+  !*** ./src/pages/page3.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ generatePageThree)\n/* harmony export */ });\nfunction generatePageThree() {\n\t// This deactivates the tab from before\n\tlet previousActiveTab = document.querySelector('.active');\n\tpreviousActiveTab.classList.remove('active');\n\tconsole.log(previousActiveTab);\n\n\t// This activates the new tab\n\tlet currentActiveTab = document.querySelector('#contact-tab');\n\tcurrentActiveTab.classList.add('active');\n\tconsole.log(currentActiveTab);\n\n\t// This removes all content from the previous page:\n\tlet allContent = document.querySelectorAll('.content');\n\tallContent.forEach((item) => {\n\t\titem.remove();\n\t});\n\n\t//This should clone and append the 3rd page\n\tconst templatePage3 = document.getElementById('page3');\n\tconst clonePage3 = templatePage3.content.cloneNode(true);\n\tconst mainContainer = document.getElementById('main-container');\n\tmainContainer.appendChild(clonePage3);\n\n\twindow.scrollTo({ top: 0, left: 0, behavior: 'instant' });\n}\n\n\n//# sourceURL=webpack://wv-berlin/./src/pages/page3.js?");
+
+/***/ }),
+
+/***/ "./src/pages/page4.js":
+/*!****************************!*\
+  !*** ./src/pages/page4.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ generatePageFour)\n/* harmony export */ });\nfunction generatePageFour() {\n\t// This deactivates the tab from before\n\tlet previousActiveTab = document.querySelector('.active');\n\tpreviousActiveTab.classList.remove('active');\n\tconsole.log(previousActiveTab);\n\n\t// This removes all content from the previous page:\n\tlet allContent = document.querySelectorAll('.content');\n\tallContent.forEach((item) => {\n\t\titem.remove();\n\t});\n\n\t// This removes all HR elements\n\tlet hrElements = document.querySelectorAll('.hr-element');\n\thrElements.forEach((item) => {\n\t\titem.remove();\n\t});\n\n\t//This should clone and append the 4th page\n\tconst templatePage4 = document.getElementById('page4');\n\tconst clonePage4 = templatePage4.content.cloneNode(true);\n\tconst mainContainer = document.getElementById('main-container');\n\tmainContainer.appendChild(clonePage4);\n\n\twindow.scrollTo({ top: 0, left: 0, behavior: 'instant' });\n}\n\n\n//# sourceURL=webpack://wv-berlin/./src/pages/page4.js?");
+
+/***/ }),
+
+/***/ "./src/assets/images/bonnie-kittle-9-nse_cRvkE-unsplash.jpg":
+/*!******************************************************************!*\
+  !*** ./src/assets/images/bonnie-kittle-9-nse_cRvkE-unsplash.jpg ***!
+  \******************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"bonnie-kittle-9-nse_cRvkE-unsplash.jpg\";\n\n//# sourceURL=webpack://wv-berlin/./src/assets/images/bonnie-kittle-9-nse_cRvkE-unsplash.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/florian-wehde-1uWanmgkd5g-unsplash.jpg":
+/*!******************************************************************!*\
+  !*** ./src/assets/images/florian-wehde-1uWanmgkd5g-unsplash.jpg ***!
+  \******************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"florian-wehde-1uWanmgkd5g-unsplash.jpg\";\n\n//# sourceURL=webpack://wv-berlin/./src/assets/images/florian-wehde-1uWanmgkd5g-unsplash.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg":
+/*!**************************************************************************!*\
+  !*** ./src/assets/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg ***!
+  \**************************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg\";\n\n//# sourceURL=webpack://wv-berlin/./src/assets/images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/ian-schneider-PAykYb-8Er8-unsplash.jpg":
+/*!******************************************************************!*\
+  !*** ./src/assets/images/ian-schneider-PAykYb-8Er8-unsplash.jpg ***!
+  \******************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"ian-schneider-PAykYb-8Er8-unsplash.jpg\";\n\n//# sourceURL=webpack://wv-berlin/./src/assets/images/ian-schneider-PAykYb-8Er8-unsplash.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/nathan-dumlao-6VhPY27jdps-unsplash.jpg":
+/*!******************************************************************!*\
+  !*** ./src/assets/images/nathan-dumlao-6VhPY27jdps-unsplash.jpg ***!
+  \******************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"nathan-dumlao-6VhPY27jdps-unsplash.jpg\";\n\n//# sourceURL=webpack://wv-berlin/./src/assets/images/nathan-dumlao-6VhPY27jdps-unsplash.jpg?");
+
+/***/ }),
+
+/***/ "./src/assets/images/vw-berlin-logo-big.png":
+/*!**************************************************!*\
+  !*** ./src/assets/images/vw-berlin-logo-big.png ***!
+  \**************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"vw-berlin-logo-big.png\";\n\n//# sourceURL=webpack://wv-berlin/./src/assets/images/vw-berlin-logo-big.png?");
+
+/***/ }),
+
+/***/ "./src/assets/images/wv-logo.png":
+/*!***************************************!*\
+  !*** ./src/assets/images/wv-logo.png ***!
+  \***************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"wv-logo.png\";\n\n//# sourceURL=webpack://wv-berlin/./src/assets/images/wv-logo.png?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
